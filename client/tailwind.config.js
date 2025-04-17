deea74a4089ab8df23e34e5fe2fb004f11262c79
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2025-04-16 21:09:59 -0400

    add heroui to tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,16 @@
+// tailwind.config.js
+const {heroui} = require("@heroui/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  darkMode: "class",
+  plugins: [heroui()],
+};
