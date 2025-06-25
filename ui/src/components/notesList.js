8d3e57e509ed841f05524da20a08823d0fdbf02e
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2025-06-25 19:15:34 -0400

    Added notes categories coming soon

--- ui/src/components/notesList.js
+++ ui/src/components/notesList.js
@@ -1,4 +1,19 @@
 export const notes = [
     { id: 'AWS_CCP', title: 'AWS CCP Study Notes', file: 'AWS_CCP.md' },
-    { id: 'note2', title: 'Note 2', file: 'note2.md' },
-];
+    { id: 'Accessibility', title: 'Accessibility', file: 'Accessibility.md' },
+    { id: 'APIs', title: 'APIs', file: 'APIs.md' },
+    { id: 'CSS', title: 'CSS', file: 'CSS.md' },
+    { id: 'DesignPatterns', title: 'Design Patterns', file: 'DesignPatterns.md' },
+    { id: 'Docker', title: 'Docker', file: 'Docker.md' },
+    { id: 'GraphQL', title: 'GraphQL', file: 'GraphQL.md' },
+    { id: 'Javascript', title: 'JavaScript', file: 'Javascript.md' },
+    { id: 'Kafka', title: 'Kafka', file: 'Kafka.md' },
+    { id: 'NestJS', title: 'NestJS', file: 'NestJS.md' },
+    { id: 'Python', title: 'Python', file: 'Python.md' },
+    { id: 'React', title: 'React', file: 'React.md' },
+    { id: 'RelationalDatabases', title: 'Relational Databases', file: 'RelationalDatabases.md' },
+    { id: 'Security', title: 'Security', file: 'Security.md' },
+    { id: 'Testing', title: 'Testing', file: 'Testing.md' },
+    { id: 'Typescript', title: 'TypeScript', file: 'Typescript.md' },
+  ];
+  
