1e926686b9a078b58cd86e0fc3a62f12a9d319d7
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2025-06-25 18:01:09 -0400

    basic styling

--- ui/src/components/NavBar/navbar.jsx
+++ ui/src/components/NavBar/navbar.jsx
@@ -1,16 +1,26 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { notes } from '../notesList';
+import logo from '../../img/codecache.png';
+import './navbar.css';
 
 export default function Navbar() {
   return (
-    <nav style={{ width: '200px', padding: '1rem', borderRight: '1px solid #ccc' }}>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
+    <>
+      <nav>
+      <img src={logo} alt="CodeCache Logo" style={{ paddingBottom: '1rem'}}/>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
         {notes.map(note => (
           <li key={note.id}>
-            <Link to={`/note/${note.id}`}>{note.title}</Link>
+            <NavLink
+              to={`/note/${note.id}`}
+              className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}
+            >
+              {note.title}
+            </NavLink>
           </li>
         ))}
       </ul>
-    </nav>
+      </nav>
+    </>
   );
 }
