196ddca82966b3691e17802aec6cc186d856c577
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2025-06-25 21:07:21 -0400

    Fix GitHub Pages build

--- ui/vite.config.mjs
+++ ui/vite.config.mjs
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/code_cache/',
+  build: {
+    outDir: '../docs', // relative to vite.config.js in /ui
+    emptyOutDir: true,
+  },
   plugins: [react()],
 });
