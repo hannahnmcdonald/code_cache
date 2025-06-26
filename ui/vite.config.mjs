1f414ee9e74792befd13dbb78e37966afd257d2e
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2025-06-25 20:28:50 -0400

    which to mjs for vite config

--- ui/vite.config.mjs
+++ ui/vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/code_cache/', // this is essential!
+  base: '/code_cache/',
   plugins: [react()],
 });
